373c5b9688c27e580eb550b6e4bd8bf97e09d36c
estoow96 <wootse96@gmail.com>
2026-04-20 18:21:04 +0800

    Create airplay_server.h header file with function prototypes

---
+++ src/airplay/airplay_server.h
@@ -0,0 +1,10 @@
+#ifndef AIRPLAY_SERVER_H
+#define AIRPLAY_SERVER_H
+
+// Function prototypes for AirPlay server
+
+void start_airplay_server();
+void stop_airplay_server();
+void handle_airplay_request();
+
+#endif // AIRPLAY_SERVER_H
