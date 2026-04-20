e0ac4d687befd3e3195a822b19f0ce7bbceba19e
estoow96 <wootse96@gmail.com>
2026-04-20 18:28:55 +0800

    Add implementation for AirPlay server

---
+++ src/airplay/airplay_server.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Implementation of the AirPlay server
+
+int main() {
+    printf("AirPlay server is running...\n");
+    // Add your server implementation here
+    return 0;
+}
